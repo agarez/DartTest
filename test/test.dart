dcbaf9fe67a8c38a51550ee2c81870b3a6bf5ec6
agarez <agarcia@maubic.es>
2013-04-29 12:19:04 +0300

    Update test.dart

--- test/test.dart
+++ test/test.dart
@@ -1,7 +1,7 @@
 library test;
 
-import 'package:unittest/html_enhanced_config.dart';
-import 'package:unittest/unittest.dart';
+//import 'package:unittest/html_enhanced_config.dart';
+//import 'package:unittest/unittest.dart';
 //import 'test_browser.dart' as browser;
 //import 'test_shared.dart' as shared;
 
