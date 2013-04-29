d0a3b32f183e10dc25320ab10df8eb172b315400
agarez <agarcia@maubic.es>
2013-04-29 12:17:15 +0300

    Update test.dart

--- test/test.dart
+++ test/test.dart
@@ -1,14 +1,14 @@
-library test_browser;
+library test;
 
 import 'package:unittest/html_enhanced_config.dart';
 import 'package:unittest/unittest.dart';
-import 'test_browser.dart' as browser;
-import 'test_shared.dart' as shared;
+//import 'test_browser.dart' as browser;
+//import 'test_shared.dart' as shared;
 
 main() {
   groupSep = ' - ';
-  useHtmlEnhancedConfiguration();
+  //useHtmlEnhancedConfiguration();
 
-  shared.register();
-  browser.register();
+  //shared.register();
+  //browser.register();
 }
